9d6ed6416ae4e3c56a7998b990461fe5edac05ec
christophersaab <chrissaab08@icloud.com>
2026-09-11 07:34:34 +0000

    EECE 230 Folder

--- EECE230/SS1.py
+++ EECE230/SS1.py
@@ -1,5 +1,5 @@
-x=int(input("Enter price of food: "))
-y=int(input("Enter price of drinks: "))
-b=int(input("Enter number of people: "))
-total=((x+y)*1.15)/b
+food=int(input("Enter price of food: "))
+drinks=int(input("Enter price of drinks: "))
+n=int(input("Enter number of people: "))
+total=((food+drinks)*1.15)/n
 print("Share=",total)
