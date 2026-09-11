8fc74457588d97c8a9dcbf92c29e6287d72fd834
christophersaab <chrissaab08@icloud.com>
2026-09-11 08:27:17 +0300

    Add files via upload

---
+++ A1P2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 08:02:08 2026
+
+@author: christophersaab
+"""
+a="The wheels on the bus go "
+b="round and round,\n"
+c="all through the town."
+print(a+3*b+a+b+c)
---
+++ A1P3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 17:05:38 2026
+
+@author: christophersaab
+"""
+from math import sqrt
+a=float(input("Enter \"a\":"))   
+b=float(input("Enter \"b\":"))
+c=float(input("Enter \"c\":"))
+if a!=0:
+    d=b*b-4*a*c
+    if d>0:
+        x1=(-b+sqrt(d))/(2*a)
+        x2=(-b-sqrt(d))/(2*a)
+        print(f"The equation has two roots :{x1} and {x2}")
+    elif abs(d)<1e-9:
+            x=(-b/2*a)
+            print(f"The equation has one root: {x}")
+    else:
+                print("The equation has no roots")
+else:
+    print("\"a\" 0can\'t be equal to \"0\"")
+    
+    
