f82418a21148cac113e2ae66f21943401861215d
christophersaab <chrissaab08@icloud.com>
2026-09-21 14:49:58 +0300

    Add files via upload

---
+++ A2P3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 17:15:01 2026
+
+@author: christophersaab
+"""
+
+x=int(input("Enter x: "))
+d=2
+IsPrime=True
+while d*d<=x:
+    if x%d==0:
+        IsPrime=False
+        print("x is not a prime")
+        break
+    d=d+1
+if IsPrime:
+    print("x is prime")
+    
