7c4c0b376e1bf6235dea4e99f4cbd34416150cf2
christophersaab <chrissaab08@icloud.com>
2026-09-21 12:25:42 +0000

    EECE 230 folder

---
+++ EECE230/A2P4.py
@@ -0,0 +1,14 @@
+x=int(input("Enter x: "))
+y=0
+for i in range (2,x+1):
+    isprime=True
+    d=2
+    while d*d<=i:
+        if i%d==0:
+            isprime=False
+            break
+        d=d+1
+    if isprime:
+            y=y+1
+print(y,x,y/x)
+
