9dbc147e254c54a61358ff0eab9df7561258de2e
christophersaab <chrissaab08@icloud.com>
2026-09-21 18:42:41 +0300

    Improve prime number check in A2P3.py
    Refactor prime checking logic to handle cases where n <= 1.

--- EECE230/A2P3.py
+++ EECE230/A2P3.py
@@ -9,12 +9,15 @@ Created on Sun Sep 20 17:15:01 2026
 x=int(input("Enter x: "))
 d=2
 IsPrime=True
-while d*d<=x:
-    if x%d==0:
-        IsPrime=False
-        print("x is not a prime")
-        break
+if n<=1:
+    print(n,"is not prime")
+else:
+    while d*d<=x:
+        if x%d==0:
+            IsPrime=False
+            print("x is not a prime")
+            break
     d=d+1
-if IsPrime:
-    print("x is prime")
+    if IsPrime:
+        print("x is prime")
     
