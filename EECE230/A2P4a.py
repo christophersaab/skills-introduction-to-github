87ef57963a1f90e4fbd3aca7c9ed89c7e561378c
christophersaab <chrissaab08@icloud.com>
2026-09-21 15:18:14 +0000

    EECE 230 folder

---
+++ EECE230/A2P4a.py
@@ -0,0 +1,23 @@
+n=int(input("Enter an integer n:"))
+i=1
+square=False
+for i in range(1,):
+    if i*i==n:
+        square=True
+        break
+if  square:
+    print("YES square:",n,"=",i,"^2")
+else:
+    print(n,"is not a square")
+
+
+
+
+    
+    
+   
+        
+    
+
+
+
--- EECE230/A2P4b.py
+++ EECE230/A2P4b.py
@@ -1,1 +1,22 @@
+n=int(input("Enter n:")) 
+low=1
+high=n
+mid=(low+high)//2
+square=False
+while low<=high:
+    if mid*mid==n:
+        square=True
+        break
+    elif mid*mid<n:
+        low=mid+1
+    elif mid*mid>n:
+        high=mid-1
+    mid=(low+high)//2
+if square:
+    print(n,"is a perfect square")
+else:
+    print(n,"is not a pefect square")
+    
+    
+    
 
